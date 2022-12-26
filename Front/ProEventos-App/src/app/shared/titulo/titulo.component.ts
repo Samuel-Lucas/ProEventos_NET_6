8fc06ce5da1e8d1130eccc95a1b5525eb60d64bc
Samuel-Lucas <samuel.moreira10@outlook.com>
2022-12-25 21:04:36 -0300

    Estilizando dinamicamente titulo para outros componentes

--- Front/ProEventos-App/src/app/shared/titulo/titulo.component.ts
+++ Front/ProEventos-App/src/app/shared/titulo/titulo.component.ts
@@ -8,8 +8,8 @@ import { Component, Input, OnInit } from '@angular/core';
 export class TituloComponent implements OnInit {
 
   @Input() titulo: string
-  @Input() subtitulo = 'Desde 2021'
-  @Input() iconClass = 'fa fa-user'
+  @Input() subtitulo = 'Desde 2022'
+  @Input() iconClass: string
   @Input() botaoListar = false
 
   constructor() { }
