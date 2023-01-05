83684fb81f9b2dabfb05cea198edee4d7b0842e6
Samuel-Lucas <samuel.moreira10@outlook.com>
2023-01-05 20:19:20 -0300

    validando formulário sub component registration

--- Front/ProEventos-App/src/app/components/user/registration/registration.component.ts
+++ Front/ProEventos-App/src/app/components/user/registration/registration.component.ts
@@ -11,18 +11,22 @@ export class RegistrationComponent implements OnInit {
   form!: FormGroup
   constructor(private fb: FormBuilder) { }
 
+  get f(): any {
+    return this.form.controls
+  }
+
   ngOnInit(): void {
     this.validation()
   }
 
   private validation(): void {
     this.form = this.fb.group({
-      primeiroNome: ['', [Validators.required, Validators.minLength(4), Validators.maxLength(10)]],
-      ultimoNome: ['', Validators.required, Validators.minLength(4), Validators.maxLength(10)],
+      primeiroNome: ['', [Validators.required, Validators.minLength(3), Validators.maxLength(10)]],
+      ultimoNome: ['', Validators.required, Validators.minLength(3), Validators.maxLength(10)],
       email: ['', Validators.required, Validators.email],
       userName: ['', [Validators.required, Validators.minLength(4), Validators.maxLength(12)]],
       senha: ['', Validators.required, Validators.minLength(6), Validators.maxLength(15)],
-      confirmaSenha: ['', [Validators.required, Validators.minLength(6), Validators.maxLength(15)]]
+      confirmaSenha: ['', [Validators.required]]
     })
   }
 }
