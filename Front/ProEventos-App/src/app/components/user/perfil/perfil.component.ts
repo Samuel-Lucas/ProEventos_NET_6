9f3ea49173c1cef10198b3679594cdbecb870d98
Samuel-Lucas <samuel.moreira10@outlook.com>
2023-01-08 23:36:54 -0300

    validando formulário componente perfil

--- Front/ProEventos-App/src/app/components/user/perfil/perfil.component.ts
+++ Front/ProEventos-App/src/app/components/user/perfil/perfil.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AbstractControlOptions, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ValidatorFields } from 'src/app/helpers/ValidatorField';
 
 @Component({
   selector: 'app-perfil',
@@ -7,9 +9,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PerfilComponent implements OnInit {
 
-  constructor() { }
+  form!: FormGroup
+  constructor(private fb: FormBuilder) { }
 
-  ngOnInit() {
+  get f(): any {
+    return this.form.controls
   }
 
+  ngOnInit() {
+    this.validation()
+  }
+
+  private validation(): void {
+
+    const formOptions: AbstractControlOptions = {
+      validators: ValidatorFields.mustMatch('senha', 'confirmaSenha')
+    }
+
+    this.form = this.fb.group({
+      titulo: ['', Validators.required],
+      primeiroNome: ['', [Validators.required, Validators.minLength(3), Validators.maxLength(10)]],
+      ultimoNome: ['', [Validators.required, Validators.minLength(3), Validators.maxLength(10)]],
+      email: ['', [Validators.required, Validators.email]],
+      telefone: ['', [Validators.required, Validators.minLength(9), Validators.maxLength(9)]],
+      funcao: ['', Validators.required],
+      descricao: ['', [Validators.required, Validators.minLength(20), Validators.maxLength(200)]],
+      senha: ['', [Validators.required, Validators.minLength(6), Validators.maxLength(15)]],
+      confirmaSenha: ['', Validators.required]
+    }, formOptions)
+  }
 }
