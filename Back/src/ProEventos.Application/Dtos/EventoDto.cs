ed3822507378c65184e9d6af5f1e88264a232de9
Samuel-Lucas <samuel.moreira10@outlook.com>
2023-01-22 23:01:22 -0300

    Data annotation required

--- Back/src/ProEventos.Application/Dtos/EventoDto.cs
+++ Back/src/ProEventos.Application/Dtos/EventoDto.cs
@@ -1,3 +1,5 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace ProEventos.Application.Dtos
 {
     public class EventoDto
@@ -5,10 +7,12 @@ namespace ProEventos.Application.Dtos
         public int Id { get; set; }
         public string Local { get; set; }
         public string DataEvento { get; set; }
+        [Required(ErrorMessage = "O campo {0} é obrigatório")]
         public string Tema { get; set; }
         public int QtdPessoas { get; set; }
         public string ImagemUrl { get; set; }
         public string Telefone { get; set; }
+        [EmailAddress(ErrorMessage = "O campo {0} precisa ser um email válido")]
         public string Email { get; set; }
     }
 }
