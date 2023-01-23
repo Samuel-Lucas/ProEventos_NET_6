d6bab74feddfd53d681ce8a6abab507cf9743c35
Samuel-Lucas <samuel.moreira10@outlook.com>
2023-01-22 23:08:19 -0300

    String validation no campo Tema

--- Back/src/ProEventos.Application/Dtos/EventoDto.cs
+++ Back/src/ProEventos.Application/Dtos/EventoDto.cs
@@ -5,14 +5,24 @@ namespace ProEventos.Application.Dtos
     public class EventoDto
     {
         public int Id { get; set; }
+
         public string Local { get; set; }
+
         public string DataEvento { get; set; }
-        [Required(ErrorMessage = "O campo {0} é obrigatório")]
+
+        [Required(ErrorMessage = "O campo {0} é obrigatório"),
+         MinLength(3, ErrorMessage = "{0} deve ter no mínimo 4 caracteres"),
+         MaxLength(50, ErrorMessage = "{0} deve ter no máximo 50 caracteres")]
         public string Tema { get; set; }
+
         public int QtdPessoas { get; set; }
+
         public string ImagemUrl { get; set; }
+
         public string Telefone { get; set; }
+
         [EmailAddress(ErrorMessage = "O campo {0} precisa ser um email válido")]
+
         public string Email { get; set; }
     }
 }
