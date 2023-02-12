728a06e8c7ae6c2aa33a16dc6a8e16ae0679bcf7
Samuel-Lucas <samuel.moreira10@outlook.com>
2023-02-11 22:53:06 -0300

    Validando campos telefone, qtd pessoas e e-mail

--- Back/src/ProEventos.Application/Dtos/EventoDto.cs
+++ Back/src/ProEventos.Application/Dtos/EventoDto.cs
@@ -6,23 +6,33 @@ namespace ProEventos.Application.Dtos
     {
         public int Id { get; set; }
 
-        public string Local { get; set; }
+        public string Local { get; set; } = null!;
 
-        public string DataEvento { get; set; }
+        public string DataEvento { get; set; } = null!;
 
         [Required(ErrorMessage = "O campo {0} é obrigatório"),
          MinLength(3, ErrorMessage = "{0} deve ter no mínimo 4 caracteres"),
          MaxLength(50, ErrorMessage = "{0} deve ter no máximo 50 caracteres")]
-        public string Tema { get; set; }
+        public string Tema { get; set; } = null!;
 
+        [Display(Name = "Quantidade de Pessoas")]
+        [Range(1, 120000, ErrorMessage ="{0} não pode ser menor que 1 e maior que 120.000")]
         public int QtdPessoas { get; set; }
 
-        public string ImagemUrl { get; set; }
+        public string ImagemUrl { get; set; } = null!;
 
-        public string Telefone { get; set; }
+        [Required(ErrorMessage="O campo {0} é obrigatório")]
+        [Phone(ErrorMessage="O campo {0} está com número inválido")]
+        public string Telefone { get; set; } = null!;
 
-        [EmailAddress(ErrorMessage = "O campo {0} precisa ser um email válido")]
+        [Required(ErrorMessage ="O Campo {0} é obrigatório.")]
+        [Display(Name = "e-mail")]
+        [EmailAddress(ErrorMessage = "É necessário ser um {0} válido")]
 
-        public string Email { get; set; }
+        public string Email { get; set; } = null!;
+
+        public IEnumerable<LoteDto>? Lotes { get; set; }
+        public IEnumerable<RedeSocialDto>? RedesSociais { get; set; }
+        public IEnumerable<PalestranteDto>? Palestrantes { get; set; }
     }
 }
