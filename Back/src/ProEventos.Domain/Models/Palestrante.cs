e9b3aa852794b74591afbb00d15be9416901078e
Samuel-Lucas <samuel.moreira10@outlook.com>
2023-05-20 14:22:43 -0300

    Removendo propriedades do identity

--- Back/src/ProEventos.Domain/Models/Palestrante.cs
+++ Back/src/ProEventos.Domain/Models/Palestrante.cs
@@ -1,13 +1,13 @@
+using ProEventos.Domain.Identity;
+
 namespace ProEventos.Domain.Models
 {
     public class Palestrante
     {
         public int Id { get; set; }
-        public string Nome { get; set; } = null!;
         public string MiniCurriculo { get; set; } = null!;
-        public string ImagemUrl { get; set; } = null!;
-        public string Telefone { get; set; } = null!;
-        public string Email { get; set; } = null!;
+        public int UserId { get; set; }
+        public User User { get; set; } = null!;
         public IEnumerable<RedeSocial>? RedesSociais { get; set; }
         public IEnumerable<PalestranteEvento>? PalestrantesEventos { get; set; } 
     }
