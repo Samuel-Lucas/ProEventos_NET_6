953132ff9b35d531fc6604e9762a4d13bbe028f0
Samuel-Lucas <samuel.moreira10@outlook.com>
2023-05-20 22:27:44 -0300

    Implementando UserPersist

---
+++ Back/src/ProEventos.Persistence/UserPersist.cs
@@ -0,0 +1,33 @@
+using Microsoft.EntityFrameworkCore;
+using ProEventos.Domain.Identity;
+using ProEventos.Persistence.Context;
+using ProEventos.Persistence.Interfaces;
+
+namespace ProEventos.Persistence
+{
+    public class UserPersist : GeralPersist, IUserPersist
+    {
+        private readonly ProEventosContexto _context;
+
+        public UserPersist(ProEventosContexto context) : base(context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<User>> GetUsersAsync()
+        {
+            return await _context.Users.ToListAsync();
+        }
+
+        public async Task<User> GetUserByIdAsync(int id)
+        {
+            return await _context.Users.FindAsync(id);
+        }
+
+        public async Task<User> GetUserByUsernameAsync(string username)
+        {
+                        return await _context.Users
+                            .SingleOrDefaultAsync(user => user.UserName == username.ToLower());
+        }
+    }
+}
