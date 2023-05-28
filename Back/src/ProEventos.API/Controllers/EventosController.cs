fc907a3772256f15d1af3750a37b7ad2461276ee
Samuel-Lucas <samuel.moreira10@outlook.com>
2023-05-28 11:12:43 -0300

    Authorize no controller de eventos

--- Back/src/ProEventos.API/Controllers/EventosController.cs
+++ Back/src/ProEventos.API/Controllers/EventosController.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using ProEventos.API.Extensions;
 using ProEventos.Application.Dtos;
@@ -6,6 +7,7 @@ using ProEventos.Application.Interfaces;
 namespace ProEventos.API.Controllers;
 
 [ApiController]
+[Authorize]
 [Route("api/[controller]")]
 public class EventosController : ControllerBase
 {
